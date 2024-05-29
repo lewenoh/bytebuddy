ae95a9a35efe669c0a272b52e426bc9702c25be3
Yuhan Wu <yw9223@willow03.doc.ic.ac.uk>
2024-05-29 18:40:05 +0100

    declared the processor structure (ProcessorDef) and defined/populated an instance of it(emulate)

--- src/emulate.c
+++ src/emulate.c
@@ -1,5 +1,25 @@
 #include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include "ProcessorDef.h"
+#define ZEROREG 0x0
+#define HALTINSTRUCTION 0x8a000000
 
 int main(int argc, char **argv) {
+  //populating the processor, p
+  struct processor p = {{0x0}, 0x0, {false, true, false, false}, {0x0}};
+  int ir = p.memory[p.pc] + (p.memory[p.pc+1] << 8) + (p.memory[p.pc+2]<<16) + (p.memory[p.pc+3]<<24);
+  //get instruction lines from bin file TODO
+
+  //emulator loop:
+  while (ir != HALTINSTRUCTION){
+        //fetch:
+      ir = p.memory[p.pc] + (p.memory[p.pc+1] << 8) + (p.memory[p.pc+2]<<16) + (p.memory[p.pc+3]<<24);
+      p.pc += 4;
+        //decode and execute: TODO
+  }
+
+  //format output TODO
+
   return EXIT_SUCCESS;
 }
