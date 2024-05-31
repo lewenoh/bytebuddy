6f41ec66a3afdb82d09e52e78afd2764e3cf19f7
Jamin Son <js5422@vertex08.doc.ic.ac.uk>
2024-05-31 21:36:14 +0100

    MakeFile made.

--- src/emulate.c
+++ src/emulate.c
@@ -3,23 +3,20 @@
 #include <stdio.h>
 #include "ProcessorDef.h"
 #define ZEROREG 0x0
-#define HALTINSTRUCTION 0x8a000000
+#define HALTINSTRUCTION 2315255808
 
 int main(int argc, char **argv) {
   //populating the processor, p
   struct processor p = {{0x0}, 0x0, {false, true, false, false}, {0x0}};
-  uint32_t ir = p.memory[p.pc];
+  uint32_t ir = p.memory[p.pc/4];
   //get instruction lines from bin file TODO
-
   //emulator loop:
   while (ir != HALTINSTRUCTION){
         //fetch:
-      ir = p.memory[p.pc];
+      ir = p.memory[p.pc/4];
       p.pc += 4;
         //decode and execute: TODO
   }
-
   //format output TODO
-
   return EXIT_SUCCESS;
 }
