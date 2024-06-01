bcd58518b73b21dfdb54d941dca7b7b8cd360a52
Yuhan Wu <yw9223@willow03.doc.ic.ac.uk>
2024-06-01 23:30:01 +0100

    finished most of dpireg, but still need to fix the CV flags

--- src/emulator/src/dpiReg.c
+++ src/emulator/src/dpiReg.c
@@ -1,53 +1,141 @@
 #include "dpiReg.h"
 
 void dpireg(struct processor p, uint32_t ir){
-    int rd = ir & 0x1f; //destination register, 5 bits
-    int rn = (ir >> 5) & 0x1f; //r2, 5 bits
-    int operand = (ir >> 10) & 0x3f; //operand , 6 bits
-    int rm = (ir >> 16) & 0x1f; //r1, 5 bits
-    int opr = (ir >> 21) & 0xf; //determines type of dpi w m, 4 bits
-    int m = (ir >> 28) & 0x1; //'', 1 bit
-    int opc = (ir >> 29) & 0x3; //opcode, determines op, 2 bits
-    int sf = ir >> 31; //1 bit, 0=32 bit regs, 1=64 bit regs
+	unsigned int rd = ir & 0x1f; //destination register, 5 bits
+	unsigned int rn = (ir >> 5) & 0x1f; //r2, 5 bits
+	unsigned int operand = (ir >> 10) & 0x3f; //operand , 6 bits
+	unsigned int rm = (ir >> 16) & 0x1f; //r1, 5 bits
+	unsigned int opr = (ir >> 21) & 0xf; //determines type of dpi w m, 4 bits
+	unsigned int m = (ir >> 28) & 0x1; //'', 1 bit
+	unsigned int opc = (ir >> 29) & 0x3; //opcode, determines op, 2 bits
+	unsigned int sf = ir >> 31; //1 bit, 0=32 bit regs, 1=64 bit regs
+	uint64_t regmask = 0xffffffffffffffff;
+	uint64_t msbmask = 0x8000000000000000;
+	int regsize = 64;
+	if (sf == 0) {
+		regmask = 0xffffffff;
+		msbmask = 0x80000000;
+		regsize = 32;
+	}
 
-    if (m == 0){
-        int shift = (opr >> 1) & 0x3;
-        int op2 = p.genregs[rm];
-        if (sf == 0) {
-            op2 = op2 & 0xffffffff;
-        }
-        switch (shift) {
-                case 0:
-                    //lsl
-                    op2 = op2 << operand
-                case 1:
-                    //lsr
-                    op2 = op2 >> operand
-                case 2:
-                    //asr
-                    
-                case 3:
-                    if (opr < 8) {
-                    //ror
-                    }
-            }
-        if ((opr & 0x1) == 1) {
-        // negate that b
-        }
+	if (m == 0){
+    		unsigned int shift = (opr >> 1) & 0x3;
+    		uint64_t op2 = p.genregs[rm] & regmask;
+    		switch (shift) {
+            		case 0:
+                		//lsl
+                		op2 = (op2 << operand) & regmask;
+             		   	break;
+            		case 1:
+                		//lsr
+                		op2 = (op2 >> operand) & regmask;
+                		break;
+            		case 2:
+                		//asr
+                		unsigned int signbit = op2 & msbmask;
+                		for (int x = 0; x<operand; x++){
+                    			op2 = (op2 >> 1) + signbit;
+                		}
+                		break;
+            		case 3:
+                		if (opr < 8) {
+                			//ror
+                    			for (int x = 0; x<operand; x++){
+                        			op2 = (op2 >> 1) + ((op2 & 0x1) << (regsize-1));
+                    			}
+                		}
+               		 	break;
+		}
+		if ((opr & 0x1) == 1) {
+    			// negate
+        		op2 = (~op2) & regmask
+   		 }
 
+		uint64_t op1 = p.genregs[rn] & regmask;
+    		if (opr >= 8) {
+    			//arithmetic
+        		if ((opc & 0x2) == 0){
+                		//add
+            			uint64_t result = op1 + op2;
+        		}
+        		else {
+                		//sub
+            			uint64_t result = op1 - op2;
+        		}
+        		if ((opc & 0x1) == 1){
+                		//set flags from result
+            			p.pstate[0] = (result & msbmask) >> (regsize-1);
+            			if (result == 0) {
+                			p.pstate[1] = 1;
+            			}
+            			else {
+                			p.pstate[1] = 0;
+           			}
 
-// below is questionable
-        if (((opr & 0x1) == 0) && (((opr >> 3) & 0x1) == 1)) {
-        //arithmetic
-        }
-        else if (opr < 8) {
-        //bit-logic
-            
-        }
-    }
-    else if ((m == 1) && (opr == 8)){
-    //multiply
-    }
+	//FIXXXXXXX
+				if ((result & regmask) == result){
+					p.pstate[2] = 0;
+				}
+				else {
+					p.pstate[2] = 1;
+				}
+				if (((op1 & msbmask) == (op2 & msbmask)) && ((result & msbmask) != (op1 & msbmask))){
+					p.pstate[3] = 1;
+				}
+				else {
+					p.pstate[3] = 0;
+				}
 
+        		}
+        	result = result & regmask;
+    		}
+    		p.genregs[rd] = (p.genregs[rd] & ~regmask) + result;
+	       	else if (opr < 8) {
+    			//bit-logic
+	        	switch (opc) {
+  	      			case 0:
+        	        		uint64_t result = op1 & op2;
+                			break;
+            			case 1:
+                			uint64_t result = op1 | op2;
+                			break;
+            			case 2:
+                			uint64_t result = op1 ^ op2;
+                			break;
+            			case 3:
+                			uint64_t result = op1 & op2;
+                			p.pstate[0] = (result & msbmask) >> (regsize-1);
+                			if (result == 0) {
+                    				p.pstate[1] = 1;
+                			}
+               				else {
+                    				p.pstate[1] = 0;
+                			}
+                			p.pstate[2] = 0;
+                			p.pstate[3] = 0;
+               				 break;
 
+			}
+    		}
+	}
+	
+	else if ((m == 1) && (opr == 8) && (opc == 0)){
+		//multiply
+    		unsigned int ra = operand & 0x1f;
+    		unsigned int x = operand >> 5;
+    		if (ra == 31){
+        		uint64_t op1 = ZEROREG;
+    		}
+    		else {
+        		uint64_t op1 = p.genregs[ra] & regmask;
+   		}
+    		uint64_t op2 = (p.genregs[rn] & regmask) * (p.genregs[rm] & regmask);
+    		if (x == 0) {
+       			 uint64_t result = op1 + op2;
+    		}
+    		else {
+        		uint64_t result = op1 - op2;
+   		}
+    		p.genregs[rd] = (p.genregs[rd] & ~regmask) + result;
+	}
 }
