19cd42cfa05eee225c36a086d644805060c383f9
Xi Ting Hoh <xth23@ic.ac.uk>
2024-06-01 23:13:53 +0100

    Completed DPI Arithmetic and Flag Set

--- src/emulator/src/dpiImm.c
+++ src/emulator/src/dpiImm.c
@@ -2,48 +2,99 @@
 #include <stdint.h>
 #include "dpiImm.h"
 #include "processor_def.h"
+#define checkBits(value, bits, mask) ((value & mask) == bits)
 
-void dpiImm( uint64_t ir, p processor ) {
-	int rd = ir & 0x1f;
-	int operand = ir >> 5 & 0x3fff;
-	int opi = ir >> 23 & 0x7;
-	int opc = ir >> 29 & 0x3;
-	int sf = ir >> 31;
+void setFlags_arithmetic(uint64_t result, int regsize) {
+	// set N flag
+	p.pstate[0] = (result >> (regsize - 1));
+
+	// set Z flag
+	p.pstate[1] = result == 0;
+}
+
+void dpiImm( struct processor p, uint32_t ir ) {
+	uint64_t rd = ir & 0x1f;
+	uint64_t operand = ir >> 5 & 0x3fff;
+	uint64_t opi = ir >> 23 & 0x7;
+	uint64_t opc = ir >> 29 & 0x3;
+	uint64_t sf = ir >> 31;
+
+	uint64_t result = 0;
+
+	if (sf == 0) {
+		int regsize = 32;
+		uint32_t regmask = 0xffffffff;
+		uint32_t msbmask = 0x80000000;
+	} else {
+		int regsize = 64;
+		uint64_t regmask = 0xffffffffffffffff;
+		uint64_t msbmask = 0x8000000000000000;
+	}
 
 	if (rd == 0x1f) {
 		return;
 	}
 
-	if (opi == 0x2) {
-		int rn = operand & 0x3fff;
-		int imm12 = (operand >> 5) & 0xfff;
-		int sh = operand >> 22;
-		if (sh = 1) {
-			Imm12 >>= 12;
+	uint64_t opimask = 0x7;
+	uint64_t opcmask = 0x3;
+
+	if (checkBits(opi, 0x2, opimask)) {
+		uint64_t rn = operand & 0x3fff;
+                uint64_t imm12 = (operand >> 5) & 0xfff;
+                uint64_t sh = operand >> 22;
+		uint64_t imm_size = 12;
+
+		if (sh == 1) {
+			imm12 >>= 12;
+			imm_size = 24;
 		}
 
 		switch (opc) {
-			case 0x0:
-			       	processor.GenReg[rd] = processor.GenReg[rn] + imm12;
-				break;
-			case 0x1: 
-				processor.GenReg[rd] = processor.GenReg[rn] + imm12;
-				setFlags();
-				break;
-			case 0x2: 
-				processor.GenReg[rd] = processor.GenReg[rn] - imm12;
-				break;
-			case 0x3: 
-				processor.GenReg[rd] = processor.GenReg[rn] - imm12;
-				setFlags();
-				break;
-			default:
-				break;
-		}
+                        case 0x0:
+                                p.genregs[rd] = p.genregs[rn] + imm12;
+                                break;
 
+                        case 0x1:
+                                p.genregs[rd] = p.genregs[rn] + imm12;
+                                setFlags_arithmetic(p.genregs[rd], regsize);
+
+                                // setting C flag
+                                p.pstate[2] = (p.genregs[rd] < p.genregs[rn] || p.genregs[rd] < imm12 );
+
+				// setting V flag
+				int imm_sign = imm12 >> (imm_size - 1);
+				int rn_sign = p.genregs[rn] >> (regsize - 1);
+				int rd_sign = p.genregs[rd] >> (regsize - 1);
+				p.pstate[3] = (imm_sign == rn_sign && rd_sign != rn_sign); 
+
+                                break;
+	
+                        case 0x2:
+                                p.genregs[rd] = processor.GenReg[rn] - imm12;
+                                break;
+
+                        case 0x3:
+                                p.genregs[rd] = processor.GenReg[rn] - imm12;
+                                setFlags_arithmetic(p.genregs[rd], regsize);
+
+				// setting C flag
+				p.pstate[2] = (p.genregs[rd] > p.genregs[rn] || p.genregs[rd] > imm12);
+
+				// setting V flag
+				int imm_sign = imm12 >> (imm_size - 1);
+                                int rn_sign = p.genregs[rn] >> (regsize - 1);
+                                int rd_sign = p.genregs[rd] >> (regsize - 1);
+				p.pstate[3] = (imm_sign == rn_sign && rd_sign != rn_sign);
+
+                                break;
+                      
+		      	default:
+                                break;
+                }
+		return;
 	}
 
-	if (opi == 101) {
+	if (checkBits(opi, 0x5, opimask)) {
 		int imm16 = operand & 0xffff;
 		int hw = operand >> 16 & 0x3;
 		int opvalue = imm16 << (hw * 0x10);
