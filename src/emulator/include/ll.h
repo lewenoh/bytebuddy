8ba4c747212af336ebed005b731a0e111f51a3dc
Yuhan Wu <yw9223@willow03.doc.ic.ac.uk>
2024-06-02 16:48:54 +0100

    first version of ll finished

---
+++ src/emulator/include/ll.h
@@ -0,0 +1,5 @@
+#include <stdint.h>
+#include "processor_def.h"
+
+void ll(struct processor p, uint32_t ir);
+
---
+++ src/emulator/src/ll.c
@@ -0,0 +1,22 @@
+#include <stdint.h>
+#include "processor_def.h"
+#include "ll.h"
+
+void ll(struct processor p, uint32_t ir){
+	unsigned int rt = ir & 0x1f;
+	unsigned int sf = ir & 0x40000000;
+	uint64_t regmask = 0xffffffffffffffff;
+        if (sf == 0) {
+                regmask = 0xffffffff;
+        }
+	
+	unsigned int msboffset = ir & 0x800000;
+	uint64_t simm19 = (ir >> 5) & 0x7ffff;
+	simm19 = simm19 * 4
+	if (msboffset > 0){
+		simm19 = simm19 + 0xffffffffffe00000;
+	}
+
+	uint64_t value = p.memory[(p.pc + simm19)/4] & regmask;
+	p.genregs[rt] = (p.genregs[rt] & ~regmask) + value;	
+}
