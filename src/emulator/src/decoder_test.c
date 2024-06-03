aa9e28eaa082d4ae07d5ec37628721ebb9500134
Yuhan Wu <yw9223@willow03.doc.ic.ac.uk>
2024-06-03 15:59:46 +0100

    cleaned up tree

--- src/emulator/src/decoder_test.c
+++
@@ -1,5 +0,0 @@
-void testCond(bool ok, char *testname ) {
-	printf("T %s: %s\n", testname, ok?"OK":"FAIL");
-}
-
-
--- src/emulator/src/emulate.c
+++ src/emulator/src/emulate.c
@@ -9,14 +9,16 @@ int main(int argc, char **argv) {
   //populating the processor, p
   struct processor p = {{0x0}, 0x0, {false, true, false, false}, {0x0}};
   uint32_t ir = p.memory[p.pc/4];
+  test_ll();
+  test_sdt();
   //get instruction lines from bin file TODO
   //emulator loop:
-  while (ir != HALTINSTRUCTION){
+  //while (ir != HALTINSTRUCTION){
         //fetch:
-      ir = p.memory[p.pc/4];
-      p.pc += 4;
+      //ir = p.memory[p.pc/4];
+    //  p.pc += 4;
         //decode and execute: TODO
-  }
+  //}
   //format output TODO
   return EXIT_SUCCESS;
 }
