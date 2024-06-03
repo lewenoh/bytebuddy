ccea0da5ed5f7ecb0d430f739cf8d4497c735b63
Xi Ting Hoh <xth23@ic.ac.uk>
2024-06-03 23:03:33 +0100

    DPI Imm tests pass

--- src/emulator/src/dpiImm.c
+++ src/emulator/src/dpiImm.c
@@ -6,53 +6,51 @@
 
 void setFlags_arithmetic(uint64_t result, int regsize, struct processor *p) {
 	// set N flag
-	p->pstate[0] = (result >> (regsize - 1));
-
+	p->pstate[0] = (result >> (regsize - 1)) & 0x1;
 	// set Z flag
-	p->pstate[1] = result == 0;
+	if (result == 0) {
+		p->pstate[1] = 1;
+	} else {
+		p->pstate[1] = 0;
+	}
+	//p->pstate[1] = (result == 0);
 }
 
 void dpiImm( struct processor *p, uint32_t ir ) {
-	uint64_t rd = ir & 0x1f;
-	uint64_t operand = ir >> 5 & 0x3fff;
-	uint64_t opi = ir >> 23 & 0x7;
-	uint64_t opc = ir >> 29 & 0x3;
-	uint64_t sf = ir >> 31;
-	int regsize = 64;
-	uint64_t regmask = 0xffffffffffffffff;
-	uint64_t msbmask = 0x8000000000000000;
+	uint64_t rd = ir & 0x1f; // bits [4:0]
+	uint64_t operand = (ir >> 5) & 0x3FFFF; // bits [22:5]
+	uint64_t opi = (ir >> 23) & 0x7; // bits [25:23]
+	uint64_t opc = (ir >> 29) & 0x3; // bits [30:29]
+	uint64_t sf = ir >> 31; // bit 31
+//	uint64_t msbmask = 0x8000000000000000 >> (sf * 32);
 
-	if (sf == 0) {
-		int regsize = 32;
-		uint32_t regmask = 0xffffffff;
-		uint32_t msbmask = 0x80000000;
-	}
+	int regsize = (sf == 1) ? 64 : 32;
+	uint64_t regmask = (sf == 1) ? 0xFFFFFFFFFFFFFFFF : 0xFFFFFFFF;
 
 	if (rd == 0x1f) {
 		return;
 	}
 
 	uint64_t opimask = 0x7;
-	uint64_t opcmask = 0x3;
+//	uint64_t opcmask = 0x3;
 
 	if (checkBits(opi, 0x2, opimask)) {
-		uint64_t rn = operand & 0x1f;
-                uint64_t imm12 = (operand >> 5) & 0xfff;
-                uint64_t sh = operand >> 22;
-		uint64_t imm_size = 12;
-
+		uint64_t rn = operand & 0x1f; // bits [5:0] of operand
+                uint64_t imm12 = (operand >> 5) & 0xfff; // bits [16:5] of operand
+                uint64_t sh = (operand >> 17); // bit 17 of operand
+		uint64_t imm_size = 12 * (sh + 1);
+		
 		if (sh == 1) {
-			imm12 >>= 12;
-			imm_size = 24;
+			imm12 = imm12 << 12;
 		}
 
 		switch (opc) {
-                        case 0x0:
-                                p->genregs[rd] = p->genregs[rn] + imm12;
+                        case 0x0: // add
+                                p->genregs[rd] = (p->genregs[rn] + imm12) & regmask;
                                 break;
 
-                        case 0x1:
-                                p->genregs[rd] = p->genregs[rn] + imm12;
+                        case 0x1: // adds
+                                p->genregs[rd] = (p->genregs[rn] + imm12) & regmask;
                                 setFlags_arithmetic(p->genregs[rd], regsize, p);
 
                                 // setting C flag
@@ -66,24 +64,39 @@ void dpiImm( struct processor *p, uint32_t ir ) {
 
                                 break;
 	
-                        case 0x2:
-                                p->genregs[rd] = p->genregs[rn] - imm12;
+                        case 0x2: // sub
+                                p->genregs[rd] = (p->genregs[rn] - imm12) & regmask;
                                 break;
 
-                        case 0x3:
-                                p->genregs[rd] = p->genregs[rn] - imm12;
-                                setFlags_arithmetic(p->genregs[rd], regsize, p);
+                        case 0x3: // subs
+
+				uint64_t result = p->genregs[rn] - imm12;
+				p->genregs[rd] = result & regmask;
+			       	setFlags_arithmetic(p->genregs[rd], regsize, p);
+			       	// setting C flag
+				p->pstate[2] = (p->genregs[rn] >= imm12);
+                
+				// setting V flag
+			       	int imm_sign_sub = (imm12 >> (regsize - 1)) & 1;
+				int rn_sign_sub = (p->genregs[rn] >> (regsize - 1)) & 1;
+				int rd_sign_sub = (result >> (regsize - 1)) & 1;
+				p->pstate[3] = (imm_sign_sub != rn_sign_sub && rn_sign_sub == rd_sign_sub);
+                
+				break;
+
+                            //    p->genregs[rd] = (p->genregs[rn] - imm12) & regmask;
+                            //    setFlags_arithmetic(p->genregs[rd], regsize, p);
 
 				// setting C flag
-				p->pstate[2] = (p->genregs[rd] > p->genregs[rn] || p->genregs[rd] > imm12);
+		//		p->pstate[2] = (p->genregs[rn] >= imm12);
 
 				// setting V flag
-				int imm_sign_sub = imm12 >> (imm_size - 1);
-                                int rn_sign_sub = p->genregs[rn] >> (regsize - 1);
-                                int rd_sign_sub = p->genregs[rd] >> (regsize - 1);
-				p->pstate[3] = (imm_sign_sub == rn_sign_sub && rd_sign_sub != rn_sign_sub);
+//				int imm_sign_sub = imm12 >> (imm_size - 1);
+//                              int rn_sign_sub = p->genregs[rn] >> (regsize - 1);
+//                                int rd_sign_sub = p->genregs[rd] >> (regsize - 1);
+//				p->pstate[3] = (imm_sign_sub != rn_sign_sub && rn_sign_sub == rd_sign_sub);
 
-                                break;
+//                                break;
                       
 		      	default:
                                 break;
@@ -93,19 +106,25 @@ void dpiImm( struct processor *p, uint32_t ir ) {
 
 	if (checkBits(opi, 0x5, opimask)) {
 		uint64_t imm16 = operand & 0xffff;
-		uint64_t hw = operand >> 16 & 0x3;
-		uint64_t opvalue = imm16 << (hw * 0x10);
+		uint64_t hw = (operand >> 16) & 0x3;
+		uint64_t opvalue = imm16 << (hw * 16);
+//		uint64_t regmask64 = 0xFFFFFFFFFFFFFFFF;
 
-		switch (opc) {
+		uint64_t result = 0;
+
+	switch (opc) {
 			case 0x0:
-				opvalue |= (imm16 << (hw * 16));
-				p->genregs[rd] = ~opvalue;
+				result = opvalue & regmask;
+				p->genregs[rd] = (~result) & regmask;
 				break;
 			case 0x2:
 				p->genregs[rd] = opvalue;
 				break;
 			case 0x3:
-				p->genregs[rd] |= opvalue;
+				uint64_t mask = 0xffff;
+				mask = ~(mask<<(hw*16));
+				p->genregs[rd] = (p->genregs[rd] & mask) | opvalue;
+				p->genregs[rd] &= regmask;
 				break;
 			default:
 				break;
--- src/emulator/src/dpiImm_add_test.c
+++ src/emulator/src/dpiImm_add_test.c
@@ -7,71 +7,198 @@
 #include "dpiImm.h"
 #include "dpiImm_add_test.h"
 #include "testcond.h"
-#include <inttypes.h> // for PRIu64
 
-void init_processor ( struct processor *p) {
+// 32B ADD without shift
+// 32 bit, add, dpiImm, arithmetic, shift_amt = 0, imm = 2, rn = 1, rd = 6
+// 0 00 100 010 0 000000000010 00001 00110 = 0x11000826
+#define ADD_TEST1 0x11000826
 
-	p->genregs[0] = 2;
-	p->genregs[1] = 5;
-	p->genregs[2] = (uint64_t)-4;
-	p->genregs[3] = (uint64_t)-10; // initialising our sample registers
-	p->genregs[31] = 0xffffffffffffffff; // have a max value register
+// 32B ADD with shift
+// 32 bit, add, dpiImm, arithmetic, shift_amt = 12, imm = 5 rn = 1, rd = 16
+// 0 00 100 010 1 000000000101 00001 10000 = 0x91401430
+#define ADD_TEST2 0x91401430
 
-	for (int i = 4; i < 31; i++) {
-		p->genregs[i] = i; // initialising the rest of the registers			
-	}    
-	p->pc = 0;
-	p->pstate[0] = false;
-	p->pstate[1] = false;
-	p->pstate[2] = false;
-	p->pstate[3] = false;
+// 32B ADDS N
+// 32 bit, adds, dpiImm, arithmetic, shift_amt = 0, imm = 1, rn = 2, rd = 7
+// 0 01 100 010 0 000000000001 00010 00111 = 0x31000447
+#define ADDS_TEST1 0x31000447
 
-	memset(p->memory, 0, sizeof(p->memory)); // sets all values of memory to 0 because we do not need memory
-}
+// 32B ADDS Z
+// 32 bit, adds, dpiImm, arithmetic, shift_amt = 0, imm = 0, rn = 0, rd = 8
+// 0 01 100 010 0 000000000000 00000 01000 = 0x31000008
+#define ADDS_TEST2 0x31000008
 
-// add test:
-void test1(struct processor *p) {
-    dpiImm(p, 0x11000826);
-    char test_result[50]; // Buffer to hold the formatted string
-    snprintf(test_result, sizeof(test_result), "Testing 2 + R1[5], expecting: 7 , got: %" PRIu64, p->genregs[6]);
-    testcond(p->genregs[6] == 7, test_result);
-}
+// 32B ADDS C
+// 32 bit, adds, dpiImm, arithmetic, shift_amt = 0, imm = 3, rn = 31, rd = 9
+// 0 01 100 010 0 000000000011 11111 01001 = 0x31000FE9
+#define ADDS_TEST3 0x31000FE9
 
-// adds tests:
-// N flag test:
-void test2(struct processor *p) {
-	dpiImm(p, 0x31000447);
-	char test_result[50];
-	snprintf(test_result, sizeof(test_result), "Testing 1 + R[2](-4), expecting: -3, got: %" PRIu64, p->genregs[7]);
-	testcond((p->genregs[7] == -3 && p->pstate[0] == 1), test_result);
-}
+// 32B ADDS C V
+// 32 bit, adds, dpiImm, arithmetic, shift_amt = 1, imm = 0xFFF, rn = 31, rd = 10
+// 0 01 100 010 1 111111111111 11111 01010 = 0x317FFFEA
+#define ADDS_TEST4 0x317FFFEA
 
-// Z flag test:
-void test3(struct processor *p) {
-	dpiImm(p, 0x31001048);
-	char test_result[100];
-        snprintf(test_result, sizeof(test_result), "Testing 4 + R[2](-4), expecting: 0, 0, 1, 0, 0 got: %" PRIu64 ", %d, %d, %d, %d", p->genregs[8], p->pstate[0], p->pstate[1], p->pstate[2], p->pstate[3]);
-        testcond((p->genregs[8] == 0 && p->pstate[0] == 0  && p->pstate[1] == 1 && p->pstate[2] == 0 && p->pstate[3] == 0), test_result);
+// 32B ADDS V
+// 32 bit, adds, dpiImm, arithmetic, shift_amt = 0, imm = 1, rn = 30, rd = 11
+// 0 01 100 010 0 000000000001 11110 01011 = 0x310007CB
+#define ADDS_TEST5 0x310007CB
 
-}
+// 32B ADDS Z C
+// 32 bit, adds, dpiImm, arithmetic, shift_amt = 0, imm = 1, rn = 31, rd = 11
+// 0 01 100 010 0 000000000001 11111 01011 = 0x310007EB
+#define ADDS_TEST6 0x310007EB
 
-// C flag test:
-void c_flag_test1(struct processor *p) {
-	dpiImm(p, 0x310013E9);
-	char test_result[100];
-        snprintf(test_result, sizeof(test_result), 
-			"Testing 4 + R[31](max), expecting: 3, 0, 0, 1, 0 got: %" PRIu64 ", %d, %d, %d, %d", 
-			p->genregs[9], p->pstate[0], p->pstate[1], p->pstate[2], p->pstate[3]);
-        testcond((p->genregs[9] == 3 && p->pstate[0] == 0  && p->pstate[1] == 0 && p->pstate[2] == 1 && p->pstate[3] == 0), test_result);
-}
+// 64B SUB without shift
+// 64 bit, sub, dpiImm, arithmetic, shift_amt = 0, imm = 1, rn = 1, rd = 15
+// 1 10 100 010 0 000000000001 00001 01111 = 0xD100042F
+#define SUB_TEST1 0xD100042F
 
+// 64B SUB with shift
+// 64 bit, sub, dpiImm, arithmetic, shift_amt = 1, imm = 4, rn = 31, rd = 15
+// 1 10 100 010 1 000000000100 11111 01111 = 0xD14013EF
+#define SUB_TEST2 0xD14013EF
+
+// 64B SUBS N
+// 64 bit, sub, dpiImm, arithmetic, shift_amt = 0, imm = 6, rn = 1, rd = 15
+// 1 11 100 010 0 000000000110 00001 01111 = 0xD100182F
+#define SUBS_TEST3 0xF100182F
+
+// 64B SUBS Z
+// 64 bit, sub, dpiImm, arithmetic, shift_amt = 0, imm = 5, rn = 1, rd = 15
+// 1 11 100 010 0 000000000101 00001 01111 = 0xD100142F
+#define SUBS_TEST4 0xF100142F
+
+// 64B SUBS C
+// 64 bit, sub, dpiImm, arithmetic, shift_amt = 0, imm = 5, rn = 1, rd = 15
+// 1 11 100 010 0 000000000101 00001 01111 = 0xF100142F
+#define SUBS_TEST5 0xF100142F
+
+// 64B SUBS V
+// 64 bit, sub, dpiImm, arithmetic, shift_amt = 1, imm = 0xFFF, rn = 3, rd = 16
+// 1 11 100 010 1 111111111111 00011 10000 = 0xF17FFC70
+#define SUBS_TEST6 0xF17FFC70
+
+// 64B SUBS C V
+
+// printf("%d, %d, %d, %d", p.pstate[0], p.pstate[1], p.pstate[2], p.pstate[3]);
+
+//movn, 32 bit reg, imm = 27, hw = 1, rd = 7
+//0001 0010 1010 0000 0000 0011 0110 0111
+#define MOVN32 0x12a00367
+
+//movn, 64 bit reg, same as movn32, but hw = 3
+//1001 0010 1110 0000 0000 0011 0110 0111
+#define MOVN64 0x92e00367
+
+//movz, 32 bit reg, same as movn32, but opc = 10
+#define MOVZ32 0x52a00367
+
+//movz, 64 bit reg, same as movn63, but opc = 10
+#define MOVZ64 0xd2e00367
+
+//movk, 32 bit reg, same as movn32, but opc = 11
+#define MOVK32 0x72a00367
+
+//movk, 64 bit reg, same as movn64, but opc = 11
+#define MOVK64 0xF2E00367
 
 void dpiImm_add_tests() {
-	struct processor p;
-	init_processor(&p);
-	test1(&p);
-	test2(&p);
-	test3(&p);
-	c_flag_test1(&p);
+	struct processor p = {{0x0}, 0x0, {false, true, false, false}, {0x0}};
+
+	// ADD_TEST1
+	p.genregs[1] = 5;
+	dpiImm(&p, ADD_TEST1);
+	testcond( p.genregs[6] == 7, "ADD without shift");
+
+	// ADD_TEST2
+	dpiImm(&p, ADD_TEST2);
+	testcond(p.genregs[16] == 20485, "ADD with shift");
+
+	// ADDS_TEST1
+	p.genregs[2] = 0x80000000;
+	dpiImm(&p, ADDS_TEST1);
+	testcond((p.genregs[7] == 2147483649 && p.pstate[0] == 1 && p.pstate[1] == 0 && p.pstate[2] == 0 && p.pstate[3] == 0), "ADDS with N flag set");
+
+	// ADDS_TEST2
+	p.genregs[0] = 0x0;
+	dpiImm(&p, ADDS_TEST2);
+	testcond( (p.genregs[8] == 0 && p.pstate[0] == 0 && p.pstate[1] == 1 && p.pstate[2] == 0 && p.pstate[3] == 0), "ADDS with Z flag set");
+
+	//ADDS_TEST3
+	p.genregs[31] = 0xFFFFFFFF;
+	dpiImm(&p, ADDS_TEST3);
+	testcond( (p.genregs[9] == 2 && p.pstate[0] == 0 && p.pstate[1] == 0 && p.pstate[2] == 1 && p.pstate[3] == 0), "ADDS with C flag set");
+
+	// ADDS_TEST4 V flag 00FFEFFF
+	dpiImm(&p, ADDS_TEST4);
+	testcond((p.genregs[10] == 0x00FFEFFF && p.pstate[0] == 0 && p.pstate[1] == 0 && p.pstate[2] == 1 && p.pstate[3] == 1), "ADDS with C V flag set");
+
+
+	// ADDS_TEST5 V
+	p.genregs[30] = 0x7FFFFFFF; 
+        dpiImm(&p, ADDS_TEST5);
+	testcond((p.genregs[11] == 0x80000000 && p.pstate[0] == 1 && p.pstate[1] == 0 && p.pstate[2] == 0 && p.pstate[3] == 1), "ADDS with V flag set");
+
+	// ADDS_TEST6
+        dpiImm(&p, ADDS_TEST6);
+	testcond((p.genregs[11] == 0x0 && p.pstate[0] == 0 && p.pstate[1] == 1 && p.pstate[2] == 1 && p.pstate[3] == 0), "ADDS with Z C flag set");
+
+	// SUB_TEST1
+	dpiImm(&p, SUB_TEST1);
+	testcond(p.genregs[15] == 4, "SUB without shift");
+
+	//SUB_TEST2
+	p.genregs[31] = 0X5000;
+	dpiImm(&p, SUB_TEST2);
+	testcond(p.genregs[15] == 0x1000, "SUB with shift");
+
+	//SUB_TEST3
+	dpiImm(&p, SUBS_TEST3);
+	testcond((p.genregs[15] == 0xffffffffffffffff && p.pstate[0] == 1 && p.pstate[1] == 0 && p.pstate[2] == 0 && p.pstate[3] == 0), "SUBS with N flag set");
+
+	//SUB_TEST4
+	dpiImm(&p, SUBS_TEST4);
+	testcond((p.genregs[15] == 0 && p.pstate[0] == 0 && p.pstate[1] == 1 && p.pstate[2] == 1 && p.pstate[3] == 0), "SUBS with Z C flag set"); 
+
+	//SUB_TEST5
+	p.genregs[1] = 7;
+	dpiImm(&p, SUBS_TEST5);
+	testcond( (p.genregs[15] == 2 && p.pstate[0] == 0 && p.pstate[1] == 0 && p.pstate[2] == 1 && p.pstate[3] == 0), "SUBS with only C flag set");
+
+	//SUBS_TEST6
+	p.genregs[3] = 0x8000000000000001;
+	dpiImm(&p, SUBS_TEST6);
+	printf("%lx", p.genregs[16]);
+	testcond ((p.genregs[16] == 0x7FFFFFFFFF001000 && p.pstate[0] == 0 && p.pstate[1] == 0 && p.pstate[2] == 0 && p.pstate[3] == 1), "SUBS with only V flag set");
+
+	//movn32
+	//0x1b << 16 = 0x1b0000;
+	//~op = 0xffe4ffff;
+	p.genregs[7] = 0x1111111111111111;
+	dpiImm(&p, MOVN32);
+	testcond(p.genregs[7] == 0xffe4ffff, "movn, 32 bit reg");
+
+	//movn64
+	//0x1b << 48= 0x1b000000000000
+	//~op = 0xffe4ffffffffffff
+	dpiImm(&p, MOVN64);
+	testcond(p.genregs[7] == 0xffe4ffffffffffff, "movn, 64 bit reg");
+
+	//movz32
+	dpiImm(&p, MOVZ32);
+	testcond(p.genregs[7] == 0x1b0000, "movz, 32 bit reg");
+
+	//movz64
+	dpiImm(&p, MOVZ64);
+	testcond(p.genregs[7] == 0x1b000000000000, "movz, 64 bit reg");
+
+	//movk32
+	p.genregs[7] = 0xaaaaaaaaaaaaaaaa;
+	dpiImm(&p, MOVK32);
+	testcond(p.genregs[7] == 0x001BAAAA, "movk, 32 bit reg");
+	
+	//movk64
+	dpiImm(&p, MOVK64);
+	testcond(p.genregs[7] == 0x001B0000001BAAAA, "movz, 64 bit reg");
 }
 
