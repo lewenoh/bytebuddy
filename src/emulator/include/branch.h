687d4fb8d79a33767d22135e31e8ba63d78b37e9
Jamin Son <js5422@vertex08.doc.ic.ac.uk>
2024-06-03 21:48:10 +0100

    Revert "Emptied master."
    This reverts commit 7d84073e5ed01a2c2660ed738b6c107f139a5e46.

---
+++ src/emulator/include/branch.h
@@ -0,0 +1,6 @@
+#ifndef BRANCH_H
+#define BRANCH_H
+#include <stdint.h>
+#include "processor_def.h"
+void branch (struct processor *p, uint32_t ir);
+#endif
---
+++ src/emulator/include/branch_test.h
@@ -0,0 +1,5 @@
+#ifndef BRANCH_TEST_H
+#define BRANCH_TEST_H
+#include "branch.h"
+void testbranch();
+#endif
