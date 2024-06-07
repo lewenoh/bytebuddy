e7f72aa4098bb57b7d6da0ab289d033e877abebe
Jamin <js5422@ic.ac.uk>
2024-06-07 19:15:00 +0100

    Added Makefile to assembler. Added table_def to include.

---
+++ src/assembler/include/table_def.h
@@ -0,0 +1,12 @@
+#ifndef TABLE_DEF_H
+#define TABLE_DEF_H
+
+// We would have to implement the array of pointers to strings dynamically ourselves.
+// #define NUMBER_OF_INSTRUCIONS 1
+extern char *instruction_table[1];
+struct label_entry {
+    char *label;
+    int address;
+};
+extern struct label_entry symbol_table[1];
+#endif 
--- src/assembler/src/assemble.c
+++ src/assembler/src/assemble.c
@@ -1,5 +1,9 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv) {
+  // .s file read -> Instruction table, Symbol Table
+  // For each instruction, classify.
+  // Encode each instruction into binary.
+  // Write binary to .bin file. 
   return EXIT_SUCCESS;
 }
