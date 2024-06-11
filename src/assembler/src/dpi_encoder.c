3fa2ee91ffea8c5367037f0cf0ff8a62286b7b1a
Yuhan Wu <yw9223@willow03.doc.ic.ac.uk>
2024-06-11 01:14:13 +0100

    completed dpiencoder, with some bugs - need to fix hex/decimal imms

--- src/assembler/src/dpi_encoder.c
+++ src/assembler/src/dpi_encoder.c
@@ -6,10 +6,25 @@
 #include "dpi_encoder.h"
 
 #define MULBASE 0x1b000000 //base for muliply instructions
-#define MSUBX 0x00008000 //x bit for msub
+#define MSUBX 0x8000 //x bit for msub
 #define WMOVEBASE 0x12800000 //base for wide move instructions, includes opi = 101
 #define MOVZOPC 0x40000000 //opc for movz = 10
 #define MOVKOPC 0x60000000 //opc for movk = 11
+#define TWOOPBASE 0xa000000 //base for two op instructions
+#define ARITHOPR 0x1000000 //opr for arithmetic operations
+#define LSR 0x400000 //shift for lsrs, 01
+#define ASR 0x800000 //shift for asrs, 10
+#define ROR 0xc00000 //shift for rors, 11
+#define ADDS 0x20000000 //opc = 01
+#define SUB 0x40000000  //opc = 10
+#define SUBS 0x60000000 //opc = 11
+#define BIC 0x200000 //opc = 00, N = 1
+#define ORR 0x400000 //01, 0
+#define ORN 0x600000 //01, 1
+#define EOR 0x800000 //10, 0
+#define EON 0xa00000 //10, 1
+#define ANDS 0xc00000 //11, 0
+#define BICS 0xe00000 //11, 1
 
 static bool codeingroup(char opcode[30], char group[12][5], int size){
 	//returns true if the opcode is in the given group
@@ -35,22 +50,13 @@ static uint32_t getreg(char reg[30]){
 	return atoi(reg);
 }
 
-static bool iscomplexop(char operand[30]){
-	//returns true if operand has shifts in it
-	for (int x = 0; x < 30 ; x++){
-		if (operand[x] == '{'){
-			return true;
-		}
-	}
-	return false;
-}
-
 uint32_t dpi_encoder(char instruction[6][30]){
 	uint32_t hexi;
 	char opcode[30];
 	strcpy(opcode, instruction[0]);
 	char mul[2][5] = {"madd", "msub"};
 	char twoop[12][5] = {"add", "adds", "sub", "subs", "and", "ands", "bic", "bics", "eor", "eon", "orr", "orn"};
+	char arith[4][5] = {"add", "adds", "sub", "subs"};
 	char oneop[9][5] = {"mov", "mul", "mneg", "neg", "negs", "mvn", "movn", "movk", "movz"}; 
 	char wmoves[3][5] = {"movn", "movk", "movz"};
 	char nodest[3][5] = {"cmp", "cmn", "tst"};
@@ -69,11 +75,65 @@ uint32_t dpi_encoder(char instruction[6][30]){
 	}
 	else if (codeingroup(opcode, twoop, 12)){
 		//2 operand instruction
-		//rd = i[1], rn = i[2], operand = i[3]
+		//rd = i[1], rn = i[2], rm = i[3], shift = i[4], amount = i[5]
+		hexi = getsf(instruction[1]) + TWOOPBASE + (getreg(instruction[3]) << 16) + (getreg(instruction[2]) << 5) + getreg(instruction[1]);
+		//M is always 0, so no need to set
+		//opr
+		if (codeingroup(opcode, arith, 4)){
+			//arith instruction
+			hexi = hexi + ARITHOPR;
+		}
+		if (strcmp(instruction[4], "lsr") == 0){
+			//no if needed for lsl, as shift = 00
+			hexi = hexi + LSR;
+		}
+		else if (strcmp(instruction[4], "asr") == 0){
+			hexi = hexi + ASR;
+		}
+		else if (strcmp(instruction[4], "ror") == 0){
+			hexi = hexi + ROR;
+		}
+
+		//opc (and N), no if needed for and or add, as opc and N are 0
+		if (strcmp(opcode, "adds") == 0) {
+			hexi = hexi + ADDS;
+		}
+		else if (strcmp(opcode, "sub") == 0) {
+			hexi = hexi + SUB;
+		}
+		else if (strcmp(opcode, "subs") == 0) {
+			hexi = hexi + SUBS;
+		}
+		else if (strcmp(opcode, "bic") == 0) {
+			hexi = hexi + BIC;
+		}
+		else if (strcmp(opcode, "orr") == 0) {
+			hexi = hexi + ORR;
+		}
+		else if (strcmp(opcode, "orn") == 0) {
+			hexi = hexi + ORN;
+		}
+		else if (strcmp(opcode, "eor") == 0) {
+			hexi = hexi + EOR;
+		}
+		else if (strcmp(opcode, "eon") == 0) {
+			hexi = hexi + EON;
+		}
+		else if (strcmp(opcode, "ands") == 0) {
+			hexi = hexi + ANDS;
+		}
+		else if (strcmp(opcode, "bics") == 0) {
+			hexi = hexi + BICS;
+		}
+
+		//operand
+		memmove(instruction[5], instruction[5]+1, 29);
+		hexi = hexi + (atoi(instruction[5]) << 10);
+
 	}
 	else if (codeingroup(opcode, oneop, 9)){
 		//single operand with destination instruction
-		//rd = i[1], operand = i[2]
+		//rd = i[1], imm = i[2], shift = i[3], amount = i[4]
 		
 		if (codeingroup(opcode, wmoves, 3)){
 			//wide moves
@@ -85,17 +145,14 @@ uint32_t dpi_encoder(char instruction[6][30]){
 				hexi = hexi + MOVKOPC;
 			} // opc for movn = 00, so no need to do else
 			
-			if (iscomplexop(operand)){
-				char splitop[3][30]; //imm, shift, amount
-				breakoperand(operand, splitop);
-				memmove(splitop[2], splitop[2] + 1, 29);
-				hexi = hexi + ((atoi(splitop[2]) / 16) << 21);	// hw
+			if (instruction[3][0] != '\0'){
+				memmove(instruction[4], instruction[4] + 1, 29);
+				hexi = hexi + ((atoi(instruction[4]) / 16) << 21);	// hw
 			}
 			
-			memmove(splitop[0], splitop[0]+3, 27);
-			uint32_t imm = strtol(splitop[0], NULL, 16);
+			memmove(instruction[2], instruction[2]+3, 27);
+			uint32_t imm = strtol(instruction[2], NULL, 16);
 			hexi = hexi + (imm << 5);
-	
 		}
 		else {
 			//aliases
@@ -103,6 +160,8 @@ uint32_t dpi_encoder(char instruction[6][30]){
 			if ((strcmp(opcode, "neg") == 0)||(strcmp(opcode, "negs") == 0)){
 				strcpy(alias[2], "x31");
 				strcpy(alias[3], instruction[2]);
+				strcpy(alias[4], instruction[3]);
+				strcpy(alias[5], instruction[4]);
 				if (strcmp(opcode, "neg") ==0){
 					strcpy(alias[0], "sub");
 				}
@@ -116,6 +175,8 @@ uint32_t dpi_encoder(char instruction[6][30]){
 				strcpy(alias[3], instruction[2]);
 				if (strcmp(opcode, "mvn")==0){
 					strcpy(alias[0], "orn");
+					strcpy(alias[4], instruction[3]);
+					strcpy(alias[5], instruction[4]);
 				}
 				else {
 					strcpy(alias[0], "orr");
@@ -157,6 +218,8 @@ uint32_t dpi_encoder(char instruction[6][30]){
 		}
 		strcpy(alias[2], instruction[1]);
 		strcpy(alias[3], instruction[2]);
+		strcpy(alias[4], instruction[3]);
+		strcpy(alias[5], instruction[4]);
 		hexi = dpi_encoder(alias);
 	}
 	else {
