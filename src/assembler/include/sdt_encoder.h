36045ca7e1cb38b156ade723e4983be7a210eb0a
Yuhan Wu <yw9223@willow03.doc.ic.ac.uk>
2024-06-11 23:41:18 +0100

    finished branch encoding

--- src/assembler/include/sdt_encoder.h
+++
@@ -1,9 +0,0 @@
-#ifndef SDT_ENCODER_H
-#define SDT_ENCODER_H
-
-#include <stdint.h>
-#include "table_def.h"
-
-uint32_t sdt_encoder(uint32_t address, char instruction[6][30]);
-
-#endif
--- src/assembler/src/assemble.c
+++ src/assembler/src/assemble.c
@@ -1,13 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include "sdt_encoder.h"
+#include "branch_encoder.h"
 
 int main(int argc, char **argv) {
   // .s file read -> Instruction table, Symbol Table
   // For each instruction, classify.
   // Encode each instruction into binary.
   // Write binary to .bin file. 
-  char eg[6][30] = {"str", "x1", "x0", "#0x120", "\0", "UO"};
-  printf("%x\n", sdt_encoder(0x10, eg));
+  char eg[6][30] = {"b", "#0x8", "\0", "\0", "\0", "\0"};
+  printf("%x\n", branch_encoder(0x0, eg));
 	return EXIT_SUCCESS;
 }
