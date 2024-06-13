f8cde21e9873137969f393ab7c4f05901d29be56
Xi Ting Hoh <xth23@ic.ac.uk>
2024-06-13 18:24:55 +0100

    initial tamagotchi

---
+++ extension/tamagotchi/display_tamagotchi.h
@@ -0,0 +1,9 @@
+//
+// Created by Xi Ting Hoh on 13/06/2024.
+//
+
+#ifndef TAMAGOTCHI_DISPLAY_TAMAGOTCHI_H
+#define TAMAGOTCHI_DISPLAY_TAMAGOTCHI_H
+void display_tamagotchi(int row, int col);
+
+#endif //TAMAGOTCHI_DISPLAY_TAMAGOTCHI_H
---
+++ extension/tamagotchi/welcome_screen.h
@@ -0,0 +1,9 @@
+//
+// Created by Xi Ting Hoh on 13/06/2024.
+//
+
+#ifndef TAMAGOTCHI_WELCOME_SCREEN_H
+#define TAMAGOTCHI_WELCOME_SCREEN_H
+void welcome_screen(int row, int col);
+
+#endif //TAMAGOTCHI_WELCOME_SCREEN_H
