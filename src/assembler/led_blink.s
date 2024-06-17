72ab72d7e481e43f5e11f89aacc901ab189cb297
Yuhan Wu <yw9223@willow03.doc.ic.ac.uk>
2024-06-17 02:20:22 +0100

    working part 3 assembly!! :)

--- src/assembler/led_blink.s
+++ src/assembler/led_blink.s
@@ -1,18 +1,29 @@
-add x0, x0, #0x3f200004
-add x1, x1, #0x00001000
-str w1, [x0]
-add x2, x2, #0x00004000
+ldr w0, address
+ldr w1, fsel
+str w1, [x0, #4]
+ldr w2, gpset
 
-str w2, [x0, #6]
-ldr x4, #1000000
-subs x4, x4, #1
-b.ne #0x18
-str w3, [x0, #6]
+loop:
+str w2, [x0, #0x1c]
+ldr w3, delayimm
+delay1:
+subs w3, w3, #1
+b.ne delay1
 
-str w2, [x0, #9]
-ldr x4, #1000000
-subs x4, x4, #1
-b.ne #0x2c
-str w3, [x0, #9]
+str w2, [x0, #0x28]
+ldr w3, delayimm
+delay2:
+subs w3, w3, #1
+b.ne delay2
 
-b #0x10
+b loop
+
+address:
+.int 0x3f200000
+fsel:
+.int 0x8000
+gpset:
+.int 0x8000
+delayimm:
+.int 0x4FFFFF
+
